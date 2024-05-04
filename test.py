afa04d16f6b1077a92e78017cb08a92995ecab95
MaxTuecke <maxtuecke@gmail.com>
2024-05-03 20:30:59 -0500

    update

---
+++ test.py
@@ -0,0 +1,22 @@
+from kubernetes import client, config
+from os import path
+import yaml, random, string, json
+import sys
+import json
+
+# Configs can be set in Configuration class directly or using helper utility
+KUB_CONFIG = "/home/ec2-user/.kube/config"
+IMAGE_ID = "maxtuecke/cs498-mp12"
+FREE_JOB_YAML = "./free_job_spec.yaml"
+PREMIUM_JOB_YAML = "./premium_job_spec.yaml"
+
+config.load_kube_config(KUB_CONFIG)
+v1 = client.CoreV1Api()
+# app.run(debug = True)
+
+job = yaml.safe_load(open(FREE_JOB_YAML))
+api_response = v1.create_namespaced_job(NAMESPACE, job)
+print(api_response)
+print(api_response.__dict__)
+
+
